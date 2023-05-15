75456c70d983071dc4d22006e455d76e3396e155
Alex Liew <liewtwoo42@gmail.com>
2023-05-15 14:54:57 -0400

    add postsIndex.jsx and index functionality with backend

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import {PostsIndex} from "./PostsIndex.jsx"
+import axios from "axios"
+import {useState, useEffect} from "react"
+
 export function Content() {
+  const [posts, setPosts] = useState([]);
+ 
+  const handleIndexPosts = () => {
+    console.log("handleIndexPosts")
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data)
+      setPosts(response.data)
+    })
+  }
+
+  useEffect(handleIndexPosts, [])
   return (
+    
     <div>
-      <h1>This is content.</h1>
+      <PostsIndex posts={posts}/>
     </div>
   )
 }
---
+++ src/PostsIndex.jsx
@@ -0,0 +1,15 @@
+export function PostsIndex(props) {
+  return(
+    <div>
+      {props.posts.map((post) => (
+        <div key= {post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          <img src={post.image} />
+
+        </div>
+      ))}
+      <p>This is Index and should be part of content.</p>
+    </div>
+  )
+}
